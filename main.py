c3ab605ca044bb6419e4235064ec0e2f9f41022e
Jan <jan@seifert-online.de>
2024-01-03 19:44:42 +0100

    idk

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 import csv
-import openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 from time import sleep
+from math import trunc
 from datetime import datetime
 from serial import Serial, PARITY_NONE, STOPBITS_ONE, EIGHTBITS
 
@@ -15,48 +18,79 @@ CODE_BAR = b"\xB1"
 CODE_NOBAR = b"\xB2"
 bytemap = {CODE_CR: b"\x22\x3B\x22", # CR -> ";"
            b"\x2E": b"\x2C"} # . -> ,
-HEADER = '"Barcode";"Manueller Code";"Scheibentyp";"Anzahl Scheiben";"Teiler-Teilerfaktor";"Anzahl Einschüsse";"Ringwert";"Teiler";"X-Abstand";"Y-Abstand";"..."'
+HEADER = '"Barcode";"Manueller Code";"Scheibentyp";"Anzahl Scheiben";"Teiler-Teilerfaktor";"Anzahl Einschüsse"'
 
 result = []
+
+def clear():
+    os.system(("cls" if os.name == "nt" else "clear"))
 
 def nowtime():
     return datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
 
-def saveData(lst):
-    wb = openpyxl.Workbook()
+def truncComma(n: str):
+    return int(trunc(float(n.replace(",", "."))))
+
+def saveData(lst: list, mode: str):
+    pattern = PatternFill(start_color="00646464", end_color="00646464", fill_type="solid")
+    wb = Workbook()
     ws = wb.active
+    values = [0]*len(lst)
     for row, line in enumerate(lst, start=1):
         for col, v2 in enumerate(csv.reader([line], delimiter=";", quotechar='"').__next__(), start=1):
-            ws.cell(row, col, v2)
+            if row != 1 and col >= 7 and col % 4 == 3:
+                if mode == "2":
+                    ws.cell(row, col, str(truncComma(v2))).fill = pattern
+                    values[row] += truncComma(v2)
+                else:
+                    ws.cell(row, col, v2).fill = pattern
+                    values[row] += v2
+            else:
+                ws.cell(row, col, v2)
+    ws.insert_cols(idx=7)
+    ws.cell(1, 7, "Gesamt").fill = pattern
+    for row, val in enumerate(values, start=2):
+        ws.cell(row, 7, val)
     wb.save(f"output_{nowtime()}.xlsx")
 
-with Serial(port="/dev/ttyUSB0", baudrate=9600, timeout=1, parity=PARITY_NONE, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS, xonxoff=False, rtscts=False) as ser:
-    try:
-        ser.write(CODE_NOBAR)
-        print("start")
-        while True:
-            ser.write(CODE_ENQ)
-            response = ser.read(1)
-            if response == CODE_NAK: # no result
+def main():
+    while True:
+        clear()
+        print("1) with decimal")
+        print("2) truncate")
+        mode = input("[1/2] >>> ")
+        if mode in ["1", "2"]:
+            break
+    with Serial(port="/dev/ttyUSB0", baudrate=9600, timeout=1, parity=PARITY_NONE, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS, xonxoff=False, rtscts=False) as ser:
+        try:
+            ser.write(CODE_NOBAR)
+            print("start")
+            while True:
+                ser.write(CODE_ENQ)
+                response = ser.read(1)
+                if response == CODE_NAK: # no result
+                    sleep(0.5)
+                    continue
+                if response == CODE_STX: # transmission start
+                    data = b"\x22" # "
+                    while True:
+                        byte = ser.read(1)
+                        if byte == CODE_ETB: # end of data
+                            break
+                        data += bytemap.get(byte, byte)
+                    data += b"\x22" # "
+                    result.append(data.decode("unicode-escape"))
+                    end = None
+                    while end != b"\x24":
+                        end = ser.read(1)
+                    ser.write(CODE_ACK) # com cycle finished
+                print("transmission finished")
                 sleep(0.5)
-                continue
-            if response == CODE_STX: # transmission start
-                data = b"\x22" # "
-                while True:
-                    byte = ser.read(1)
-                    if byte == CODE_ETB: # end of data
-                        break
-                    data += bytemap.get(byte, byte)
-                data += b"\x22" # "
-                result.append(data.decode("unicode-escape"))
-                end = None
-                while end != b"\x24":
-                    end = ser.read(1)
-                ser.write(CODE_ACK) # com cycle finished
-            print("transmission finished")
-            sleep(0.5)
-    except KeyboardInterrupt:
-        print("KeyboardInterrupt")
-        ser.write(CODE_EXIT) # set device inactive
-        saveData([HEADER]+result)
-        sleep(1)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            ser.write(CODE_EXIT) # set device inactive
+            saveData([HEADER]+result, mode)
+            sleep(1)
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import csv
 import openpyxl
+from math import trunc
 
 # Example list of CSV-compatible lines
 csv_data = [
     '"Name";"Age";"Location"',
-    '"John";"25";"New York"',
-    '"Alice";"30";"Los Angeles";"testabc";"testdef"',
-    '"Bob";"28";"Chicago"'
+    '"John";"25,3";"New York"',
+    '"Alice";"30,5";"Los Angeles";"testabc";"testdef"',
+    '"Bob";"28,0";"Chicago"'
 ]
 
 # Create a new Excel workbook
@@ -14,6 +15,9 @@ wb = openpyxl.Workbook()
 ws = wb.active
 for row, line in enumerate(csv_data, start=1):
     for col, v2 in enumerate(csv.reader([line], delimiter=";", quotechar='"').__next__(), start=1):
-        ws.cell(row, col, v2)
+        if row != 1 and col == 2:
+            ws.cell(row, col, str(trunc(float(v2.replace(",", ".")))))
+        else:
+            ws.cell(row,)
 
 wb.save("test.xlsx")
