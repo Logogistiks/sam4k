391d6ddc4b8097a40005234b391484020689e812
Jan <92688175+Logogistiks@users.noreply.github.com>
2024-10-22 17:05:37 +0200

    added clear script

---
+++ clear.py
@@ -0,0 +1,16 @@
+import os
+from time import sleep
+import sys
+
+if __name__ == "__main__":
+    if input("Clear xlsx output files? [y/n] >>> ").lower() != "y":
+        print("cancelled")
+        sleep(1)
+        sys.exit()
+
+    for file in os.listdir():
+        if file.endswith(".xlsx"):
+            os.remove(file)
+            #print(f"Removed {file}")
+    print("Cleared output files!")
+    sleep(1)
